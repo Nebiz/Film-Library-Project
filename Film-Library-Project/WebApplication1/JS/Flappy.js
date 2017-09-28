ec8d2729ab8bfca07ae0bf5b525b96839a2d1864
Benjamin Tourigny <ben.tourigny@gmail.com>
2017-09-28 00:10:24 -0400

    Avancement Site Web, Test WebGame

---
+++ Film-Library-Project/WebApplication1/JS/Flappy.js
@@ -0,0 +1,25 @@
+﻿window.onload = function () {
+
+    canv = document.getElementById("myFlappy");
+    ctx = canv.getContext("2d");
+    
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canv.width, canv.height);
+
+    //var bird = new Bird(canv, ctx);
+    //bird.show();
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(canv.width / 4, (canv.height / 2) - 12, 24, 24);
+
+}
+
+//function Bird(canv, ctx) {
+//    this.y = height / 2;
+//    this.x = width / 3;
+
+//    this.show = function () {
+//        ctx.fillStyle = "white";
+//        ctx.fillRect(canv.width / 3, canv.height / 2, 25, 25);
+//    }
+//}
